65ded7ac452422acf87269461987c60e756992b7
Vijay vardhan <vijayvardhan.killi@gmail.com>
2025-02-18 15:07:45 +0530

    set debug = False in backend

--- backend/youtube_api/youtube_api/settings.py
+++ backend/youtube_api/youtube_api/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY','default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1','youtube-playlist-duration-checker.onrender.com']
 
