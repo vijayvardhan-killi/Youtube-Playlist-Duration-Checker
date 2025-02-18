a7425d01f264a18a82d63443bedebfb5915d7c82
Vijay vardhan <vijayvardhan.killi@gmail.com>
2025-02-18 12:02:46 +0530

    set footer and backend , Changed APi endpoint url to Backend Server

--- frontend/youtube-duration-app/src/components/Footer.jsx
+++ frontend/youtube-duration-app/src/components/Footer.jsx
@@ -4,6 +4,7 @@ const Footer = () => {
   return ( 
     <div className='bg-youtube-black text-youtube-light-gray '>
       <p className='text-xl text-center p-4'>powerd by Youtube API</p>
+      <p className='text-center p-3'>Tracking your time, one playlist at a time.</p>
     </div>
   )
 }
--- frontend/youtube-duration-app/src/services/api.js
+++ frontend/youtube-duration-app/src/services/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://127.0.0.1:8000";
+// const BASE_URL = "http://127.0.0.1:8000"; uncomment for dev testing
+const BASE_URL = 'https://youtube-playlist-duration-checker.onrender.com';
 
 export default async function getData(video_id){
     const request = `${BASE_URL}/api/playlist_duration/?playlist_id=${video_id}`;
